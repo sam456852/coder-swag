c6f9fa8a51229ff3044b5ae3b1017c4918fa9f1a
sam456852 <weixiangzhang@u.northwestern.edu>
2017-11-02 01:59:25 -0500

    Added table cell

---
+++ coder-swag/coder-swag/Services/DataService.swift
@@ -0,0 +1,23 @@
+//
+//  DataService.swift
+//  coder-swag
+//
+//  Created by Weixiang Zhang on 11/2/17.
+//  Copyright © 2017 Weixiang Zhang. All rights reserved.
+//
+
+import Foundation
+class DataService {
+    static let instance = DataService()
+    
+    private let categories = [
+        Category(title: "SHIRTS", imageName: "shirts.png"),
+        Category(title: "HOODIES", imageName: "hoodies.png"),
+        Category(title: "HATS", imageName: "hats.png"),
+        Category(title: "DIGITAL", imageName: "digital.png")
+    ]
+    
+    func getCategories() -> [Category]{
+        return categories
+    }
+}
