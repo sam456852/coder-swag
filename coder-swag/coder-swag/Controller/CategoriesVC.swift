492b7a32869fadbe87989eb6e47d0f1d4e952e21
sam456852 <weixiangzhang@u.northwestern.edu>
2017-11-02 00:48:09 -0500

    Added assets

---
+++ coder-swag/coder-swag/Controller/CategoriesVC.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Weixiang Zhang on 11/2/17.
+//  Copyright © 2017 Weixiang Zhang. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+    
+    @IBOutlet weak var categoryTable: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
