1dd8880ebb0d7aa7d8091569e2e5865f9a368759
sam456852 <weixiangzhang@u.northwestern.edu>
2017-11-02 23:32:41 -0500

    Added table cell

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Weixiang Zhang on 11/2/17.
+//  Copyright © 2017 Weixiang Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
